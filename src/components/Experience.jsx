fb2774b406f516bdca9260cf0b6192f7c565fbfd
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2024-08-13 17:12:55 -0500

    se edita orbitControls

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,7 +5,7 @@ import { PCRoom } from "./myRoom";
 export const RoomExperience = () => {
   return (
     <>
-      <OrbitControls enableZoom={false} />
+      <OrbitControls />
       <ambientLight intensity={1.5} />
       <directionalLight intensity={2} position={[1, 5, 5]} castShadow />
       <ScrollControls pages={1} damping={2} />
